ecc277ff13f95be25931c66918137dcbfe6d1e77
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-05 15:49:50 +0000

    got the links working

--- src/app.js
+++ src/app.js
@@ -5,8 +5,21 @@ import{fleet} from './fleet-data.js';
 import{FleetDataService} from './services/fleet-data-service.js'
 import {Button} from './ui/button.js';
 import{Image} from './ui/image.js'
-let b = new Button('Click Me');
-b.appendToElement($('body'));
+import{TitleBar} from './ui/title-bar.js'
+let tb = new TitleBar('Shorty Get Down');
+tb.addLink('Home', '');
+tb.addLink('Cars', '');
+tb.addLink('Drones', '');
+tb.addLink('Map', '');
+tb.appendToElement($('body'));
+
+
 
 let i = new Image('images/drone.jpg');
 i.appendToElement($('body'));
+
+let b = new Button('Click Me');
+b.appendToElement($('body'));
+
+
+
