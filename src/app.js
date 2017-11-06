8cb2a14ac5099cca533c857a92172c38eb0917f5
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-06 06:16:10 +0000

    added table populating data

--- src/app.js
+++ src/app.js
@@ -4,22 +4,29 @@ import {Drone} from './classes/drone.js';
 import{fleet} from './fleet-data.js';
 import{FleetDataService} from './services/fleet-data-service.js'
 import {Button} from './ui/button.js';
-import{Image} from './ui/image.js'
-import{TitleBar} from './ui/title-bar.js'
-let tb = new TitleBar('Shorty Get Down');
-tb.addLink('Home', '');
-tb.addLink('Cars', '');
-tb.addLink('Drones', '');
-tb.addLink('Map', '');
-tb.appendToElement($('body'));
+import{Image} from './ui/image.js';
+import{TitleBar} from './ui/title-bar.js';
+import{DataTable} from './ui/data-table.js';
+// let tb = new TitleBar('Shorty Get Down');
+// tb.addLink('Home', '');
+// tb.addLink('Cars', '');
+// tb.addLink('Drones', '');
+// tb.addLink('Map', '');
+// tb.appendToElement($('body'));
 
+let headers = "License Make Model Miles".split(' ');
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+let dt = new DataTable(headers, dataService.cars);
+dt.appendToElement($('body'));
+console.log(fleet);
 
-
+/*
 let i = new Image('images/drone.jpg');
 i.appendToElement($('body'));
 
 let b = new Button('Click Me');
 b.appendToElement($('body'));
 
-
+*/
 
